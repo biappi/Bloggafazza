38d7dc216298041195704c1d3b6c8c3d79763ef4
Biappi <antonio.malara@gmail.com>
2011-12-01 23:46:47 +0100

    Initial Commit

---
+++ Bloggafazza/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Bloggafazza
+//
+//  Created by Antonio Malara on 01/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
