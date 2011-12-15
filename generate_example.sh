66e6fe11e6758ef3abd36374de78af50e15f2130
Biappi <antonio.malara@gmail.com>
2011-12-15 22:39:45 +0100

    Added an example

---
+++ generate_example.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Just the example invocation, yes, i know, it's less than ideal
+
+python bloggafazza.py -d example/entries/ -t example/templates/ -o example/output/ -r example/resources/ 
