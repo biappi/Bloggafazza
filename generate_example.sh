2d71ed80be1405e557514b5e7837ca65b2ce7dd8
root <root@tavor.(none)>
2012-02-28 16:29:36 +0000

    Deleted the example, more clean now, but branch is nonsensical, who cares?

--- generate_example.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Just the example invocation, yes, i know, it's less than ideal
-
-python bloggafazza.py -d example/entries/ -t example/templates/ -o example/output/ -r example/resources/ 
