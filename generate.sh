0c3501328148ef476a9ad0550ab6661252eaa95d
Antonio "Willy" Malara <antonio.malara@gmail.com>
2012-06-04 00:41:17 +0200

    Forced deletion on generate.sh

--- generate.sh
+++ generate.sh
@@ -2,4 +2,4 @@
 
 # Just the example invocation, yes, i know, it's less than ideal
 
-python ../bloggafazza.py -d entries/ -t templates/ -o output/ -r resources/ 
+python bloggafazza.py --ForceDeletion -d entries/ -t templates/ -o output/ -r resources/ 
